f2778f97d2856b3fe35afc5f74cac343b5cdb241
Pedro Basilio <phbasilio6903@gmail.com>
2025-05-10 16:20:49 -0300

    setup project

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify
 import requests
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/api/solar_cep/<string:cep>/<float:peakpower>', methods=['GET'])
 def get_solar_data_from_cep(cep, peakpower):
